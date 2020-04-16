4925d8334333eceb8f5acf68de7b143e4aee1d9b
Rommel Martinez <ebzzry@icloud.com>
2020-04-16 20:42:03 +0800

    symbols: refactor

--- symbols.lisp
+++ symbols.lisp
@@ -2,16 +2,58 @@
 
 (uiop:define-package #:marie/symbols
   (:use #:cl)
-  (:export #:define-constant
+  (:export #:defun*
+           #:defvar*
+           #:defparameter*
+           #:define-constant
            #:define-constant*
            #:define-alias
-           #:defun*
            #:symbols
            #:with-gensyms
            #:macroexpand*
            #:symbol*))
 
 (in-package #:marie/symbols)
+
+(defmacro defun* (spec args &rest body)
+  "Define a function with aliases and optionally export the names. SPEC is
+either a single symbol, or a list where the first element is the name of the
+function and the rest are aliases. If the last element of SPEC is T, the
+function names, including the aliases, are exported."
+  (let ((id (if (consp spec) spec (list spec))))
+    (destructuring-bind (name &rest handles)
+        id
+      (let* ((exportp (equal (car (reverse handles)) t))
+             (aliases (if exportp (butlast handles) handles)))
+        `(progn
+           (defun ,name ,args ,@body)
+           ,(when exportp `(export ',name))
+           ,@(loop :for alias :in aliases
+                   :when alias
+                   :collect `(progn (define-alias ,name ,alias)
+                                    ,(when exportp `(export ',alias)))))))))
+
+(defmacro defvar* (spec &rest body)
+  "Define a special variable by DEFVAR and optionally export the name. SPEC is either a
+single symbol, or a list where the second element of the list is a boolean
+indicating whether to export the symbol or not."
+  (let ((id (if (consp spec) spec (list spec))))
+    (destructuring-bind (name &optional exportp)
+        id
+      `(progn
+         (defvar ,name ,@body)
+         ,(when exportp `(export ',name))))))
+
+(defmacro defparameter* (spec &rest body)
+  "Define a special variable by DEFPARAMETER and optionally export the name. SPEC is either a
+single symbol, or a list where the second element of the list is a boolean
+indicating whether to export the symbol or not."
+  (let ((id (if (consp spec) spec (list spec))))
+    (destructuring-bind (name &optional exportp)
+        id
+      `(progn
+         (defparameter ,name ,@body)
+         ,(when exportp `(export ',name))))))
 
 (defmacro define-constant (name value &optional doc)
   "Create a constant only if it hasn’t been bound or created, yet. SBCL complains
@@ -28,37 +70,21 @@ about constants being redefined, hence, this macro."
     (when restart
       (invoke-restart restart))))
 
-(defmacro define-constant* (name value &optional doc)
+(defmacro define-constant* (spec &rest body)
   "Bind NAME to VALUE and only change the binding after subsequent calls to the
 macro."
-  `(handler-bind #+sbcl ((sb-ext:defconstant-uneql #'call-continue-restart))
-                 #-sbcl ((simple-error #'call-continue-restart))
-     (defconstant ,name ,value
-       ,@(when doc (list doc)))))
+  (let ((id (if (consp spec) spec (list spec))))
+    (destructuring-bind (name &optional exportp)
+        id
+      `(handler-bind #+sbcl ((sb-ext:defconstant-uneql #'call-continue-restart))
+                     #-sbcl ((simple-error #'call-continue-restart))
+         ,(when exportp `(export ',name))
+         (defconstant ,name ,@body)))))
 
 (defmacro define-alias (name alias)
-  "Define ALIAS as an alternate name for NAME."
+  "Define ALIAS as an alternate name for function NAME."
   `(defun ,alias (&rest args)
      (apply #',name args)))
-
-(defmacro defun* (spec args &rest body)
-  "Define a function with aliases and optionally export the symbols. SPEC is a
-list where the first element is the name of the function and the rest are
-aliases. If the last element of SPEC is T, the function names, including the
-aliases, are exported."
-  (let ((fid (if (consp spec) spec (list spec))))
-    (destructuring-bind (name &rest handles)
-        fid
-      (let* ((exportp (equal (car (reverse handles)) t))
-             (aliases (if exportp (butlast handles) handles)))
-        `(progn
-           (defun ,name ,args ,@body)
-           ,(when exportp `(export ',name))
-           ,@(loop :for alias :in aliases
-                   :when alias
-                   :collect `(progn (defun ,alias (&rest args)
-                                      (apply #',name args))
-                                    ,(when exportp `(export ',alias)))))))))
 
 (defmacro symbols (package &key (location :external-symbols))
   "Collect symbols in a package. Prints external symbols by default."
@@ -68,7 +94,7 @@ aliases, are exported."
 
 ;;; From Practical Common Lisp (2005)—Peter Seibel
 (defmacro with-gensyms ((&rest names) &body body)
-  `(let ,(loop :for name :in names :collect `(,name (gensym)))
+  `(let ,(loop :for napme :in names :collect `(,name (gensym)))
      ,@body))
 
 (defmacro macroexpand* (form)
