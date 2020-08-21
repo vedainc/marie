086926a53e81f94e4d4035dad7a07e8d59792033
Rommel Martinez <ebzzry@icloud.com>
2020-08-21 20:43:01 +0800

    symbol: refactor

--- symbols.lisp
+++ symbols.lisp
@@ -21,61 +21,64 @@
 
 (in-package #:marie/symbols)
 
+(defun cexport (symbol)
+  "Export SYMBOL only if it matches a certain criteria."
+  (let ((name (string symbol)))
+    (unless (and (not (zerop (length name)))
+                 (member (elt name 0) '(#\%) :test #'equal))
+      (export symbol))))
+
 (defmacro def (spec args &rest body)
   "Define a function with aliases and export the names. SPEC is either a single symbol, or a list
 where the first element is the name of the function and the rest are aliases."
-  (let ((id (if (consp spec) spec (list spec))))
-    (destructuring-bind (name &rest aliases)
-        id
-      `(progn
-         (defun ,name ,args ,@body)
-         (export ',name)
-         ,@(loop :for alias :in aliases
-                 :when alias
-                 :collect `(progn (setf (fdefinition ',alias) (fdefinition ',name))
-                                  (export ',alias)))))))
+  (destructuring-bind (name &rest aliases)
+      (uiop:ensure-list spec)
+    `(progn
+       (defun ,name ,args ,@body)
+       (cexport ',name)
+       ,@(loop :for alias :in aliases
+               :when alias
+               :collect `(progn (setf (fdefinition ',alias) (fdefinition ',name))
+                                (export ',alias))))))
 
 (defmacro defm (spec &rest body)
   "Define a macro with aliases and export the names. SPEC is either a single symbol, or a list where
 the first element is the name of the function and the rest are aliases."
-  (let ((id (if (consp spec) spec (list spec))))
-    (destructuring-bind (name &rest aliases)
-        id
-      `(progn
-         (defmacro ,name ,@body)
-         (export ',name)
-         ,@(loop :for alias :in aliases
-                 :when alias
-                 :collect `(progn (setf (macro-function ',alias) (macro-function ',name))
-                                  (export ',alias)))))))
+  (destructuring-bind (name &rest aliases)
+      (uiop:ensure-list spec)
+    `(progn
+       (defmacro ,name ,@body)
+       (cexport ',name)
+       ,@(loop :for alias :in aliases
+               :when alias
+               :collect `(progn (setf (macro-function ',alias) (macro-function ',name))
+                                (export ',alias))))))
 
 (defmacro defv (spec &rest body)
   "Define a special variable by DEFVAR with aliases and export the names. SPEC is either a single
 symbol, or a list where the first element is the name of the function and the rest are aliases."
-  (let ((id (if (consp spec) spec (list spec))))
-    (destructuring-bind (name &rest aliases)
-        id
-      `(progn
-         (defvar ,name ,@body)
-         (export ',name)
-         ,@(loop :for alias :in aliases
-                 :when alias
-                 :collect `(progn (defvar ,alias ,@body)
-                                  (export ',alias)))))))
+  (destructuring-bind (name &rest aliases)
+      (uiop:ensure-list spec)
+    `(progn
+       (defvar ,name ,@body)
+       (cexport ',name)
+       ,@(loop :for alias :in aliases
+               :when alias
+               :collect `(progn (defvar ,alias ,@body)
+                                (export ',alias))))))
 
 (defmacro defp (spec &rest body)
   "Define a special variable by DEFPARAMETER and export the names. SPEC is either a single symbol,
 or a list where the first element is the name of the function and the rest are aliases."
-  (let ((id (if (consp spec) spec (list spec))))
-    (destructuring-bind (name &rest aliases)
-        id
-      `(progn
-         (defparameter ,name ,@body)
-         (export ',name)
-         ,@(loop :for alias :in aliases
-                 :when alias
-                 :collect `(progn (defparameter ,alias ,@body)
-                                  (export ',alias)))))))
+  (destructuring-bind (name &rest aliases)
+      (uiop:ensure-list spec)
+    `(progn
+       (defparameter ,name ,@body)
+       (cexport ',name)
+       ,@(loop :for alias :in aliases
+               :when alias
+               :collect `(progn (defparameter ,alias ,@body)
+                                (export ',alias))))))
 
 (defun call-continue-restart (condition)
   "Call the continue restart on CONDITION."
