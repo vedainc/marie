413919b4a3f6468d286607459033d61de484f042
Rommel Martinez <ebzzry@icloud.com>
2023-08-03 21:00:04 +0800

    [symbols] add new function

--- src/driver.lisp
+++ src/driver.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- mode: lisp; syntax: common-lisp; base: 10; coding: utf-8-unix; external-format: (:utf-8 :eol-style :lf); -*-
 ;;;; driver.lisp: top-level definitions for exporting marie symbols
 
-(uiop:define-package #:marie/driver
+(uiop:define-package #:marie/src/driver
   (:nicknames :marie)
   (:use :uiop/common-lisp)
   (:use-reexport #:marie/src/definitions
