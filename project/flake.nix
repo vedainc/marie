d564a9593430375ea18538a28f62ff5970810a00
Rommel Martinez <ebzzry@icloud.com>
2025-02-06 19:57:53 +0800

    [flake.nix] fix f-typo

--- project/flake.nix
+++ project/flake.nix
@@ -4,8 +4,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.system;
+      let pkgs = nixpkgs.legacyPackages.${system};
       in { devShells = import ./shells.nix { inherit nixpkgs pkgs; }; });
 }
