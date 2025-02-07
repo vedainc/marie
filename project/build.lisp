05adbeacfed8f5de19b3f4794c119cbbdbf6ea4b
Rommel Martinez <ebzzry@icloud.com>
2025-02-07 14:51:18 +0800

    [project] refactor makefile

--- project/build.lisp
+++ project/build.lisp
@@ -1,10 +1,11 @@
-;;;; build.lisp --- build the ${project} executable
+;;; build.lisp --- build the ${project} executable
 
 (require 'asdf)
 (defun cwd-name ()
   (multiple-value-bind (type list &rest rest)
       (uiop:split-unix-namestring-directory-components
        (namestring (uiop:getcwd)))
+    (declare (ignore rest))
     (car (last list))))
 (defun cwd-keyword () (intern (cwd-name) (find-package :keyword)))
 (defun home (path) (merge-pathnames path (user-homedir-pathname)))
