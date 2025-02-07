78c3cc83a1d6231c230ac1fd0ec277e88532c6e6
Rommel Martinez <ebzzry@icloud.com>
2025-02-07 12:29:47 +0800

    [cli] fix version generation

--- project/cli.lisp
+++ project/cli.lisp
@@ -7,37 +7,45 @@
 
 (in-package #:${project}/src/cli)
 
+(defk- +project-name+
+  "${project}"
+  "The name of the project.")
+
+(defk- +project-version+
+  "0.0.0"
+  "The version number of the project.")
+
 (def- cli-opts ()
   "Return the list of options."
   (list
    (clingon:make-option
     :flag
-    :description "Display usage"
+    :description "Display short help"
     :short-name #\h
-    :long-name "help"
     :key :help)
    (clingon:make-option
     :string
-    :description "Foo"
-    :short-name #\f
-    :long-name "foo"
-    :initial-value "foo-bar-baz"
-    :key :foo)))
+    :description "Project name"
+    :short-name #\p
+    :long-name "project"
+    :initial-value +project-name+
+    :key :project)))
 
 (def- cli-handler (cmd)
   "The top-level handler function."
   (let ((args (clingon:command-arguments cmd))
-        (foo (clingon:getopt cmd :foo)))
-    (princ foo)
-    (princ (lisp-implementation-type))
-    (princ (lisp-implementation-version))))
+        (project (clingon:getopt cmd :project)))
+    (princ project)
+    (terpri)
+    (princ +project-version+)
+    (terpri)))
 
 (def- cli-command ()
   "A command to ${project}."
   (clingon:make-command
    :name "${project}"
    :description "${project}"
-   :version (:read-file-form #P"version.sexp")
+   :version +project-version+
    :authors '("${author} <${email}>")
    :license ""
    :options (cli-opts)
